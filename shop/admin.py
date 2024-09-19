71d5f91176e763266beaf25013aa6b5635f9f5f3
SHADIK <shadik.sk420@gmail.com>
2024-09-20 04:14:42 +0600

    updated admin panel

--- shop/admin.py
+++ shop/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Products, Order
-
-# Register your models here.
 
 @admin.register(Products)
 class ProductsAdmin(admin.ModelAdmin):
-    list_display = ('title', 'price', 'discount_price', 'category')  # Columns to display in the admin list view
-    search_fields = ('title', 'category')  # Search functionality
-    list_filter = ('category',)  # Filter by category
+    list_display = ('id', 'title', 'price', 'discount_price', 'category','description', 'image_preview')
+    search_fields = ('title', 'category', 'description')
+    list_filter = ('category',)
+    list_editable = ('price', 'discount_price')
+    ordering = ('id',)
+    readonly_fields = ('image_preview',)
+
+    def image_preview(self, obj):
+        return format_html('<img src="{}" width="50" height="50" />', obj.image)
+    image_preview.short_description = 'Image Preview'
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ('name', 'email', 'city', 'state', 'total')  # Columns to display in the admin list view
-    search_fields = ('name', 'email', 'city')  # Search functionality
-    list_filter = ('state',)  # Filter by state
+    list_display = ('id', 'name', 'email', 'city', 'state', 'total')
+    search_fields = ('name', 'email', 'city', 'state')
+    list_filter = ('state',)
+    ordering = ('id',)
